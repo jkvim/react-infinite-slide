94e6cac3fce466fada506977067cd28af53ba39c
jkvim <jkvim@outlook.com>
2016-11-25 12:01:29 +0800

    component: add arrows and dots implement

---
+++ src/arrows.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+function Arrow({ direction, onClick }) {
+  const style = {
+    wrapper: {
+      position: 'absolute',
+      top: '50%',
+      height: '40px',
+      width: '30px',
+      textAlign: 'center',
+      background: 'rgba(0, 0, 0, 0.6)',
+      boxSizing: 'border-box',
+      zIndex: 1
+    },
+    arrow: {
+      width: '12px',
+      height: '12px',
+      borderTop: '5px solid #fff',
+      borderLeft: '5px solid #fff',
+      borderRadius: '3px',
+      display: 'block',
+      margin: '0 auto',
+      marginTop: '10px'
+    }
+  };
+  if (direction === 'left') {
+    style.wrapper.left = '0';
+    style.arrow.transform = 'rotate(-45deg)';
+  } else if (direction === 'right') {
+    style.wrapper.right = '0';
+    style.arrow.transform = 'rotate(135deg)';
+  }
+  return (
+    <div className={`arrow-${direction}`} style={style.wrapper} onClick={onClick} >
+      <div style={style.arrow}></div>
+    </div>
+  )
+}
+
+const ArrowLeft = ({ onClick }) => {
+  return (
+    <Arrow direction="left" onClick={onClick} />
+  );
+}
+
+const ArrowRight = ({ onClick }) => {
+  return (
+    <Arrow direction="right" onClick={onClick} />
+  );
+}
+
+export {
+  ArrowLeft,
+  ArrowRight
+};
---
+++ src/dots.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+export default class Dots extends React.Component {
+  onClick(key) {
+    return () => {
+      if (typeof this.props.onDotsClick === 'function') {
+        this.props.onDotsClick(key)
+      }
+    }
+  }
+
+  getDots() {
+    const dots = [];
+    console.log(this.props.activeDot)
+    for (let i = 0; i < this.props.length || 0; i++) {
+      const style = {
+        width: '5px',
+        height: '5px',
+        margin: '0 10px',
+        borderRadius: '50%',
+        display: 'inline-block',
+        background: 'rgba(0, 0, 0, 0.3)',
+      };
+
+      if (i === this.props.activeDot) {
+        style.background = 'rgba(0, 0, 0, 1)';
+      }
+
+      dots.push(
+        <li onClick={this.onClick(i)}
+          style={style}
+          key={i}
+          ></li>
+      );
+    }
+    return dots;
+  }
+
+  render() {
+    const dots = this.getDots();
+    const style = {
+      zIndex: 2,
+      left: '0',
+      margin: '0',
+      padding: '0',
+      width: '100%',
+      bottom: '10px',
+      listStyle: 'none',
+      textAlign: 'center',
+      position: 'absolute',
+    };
+    return (
+      <ul style={style}>
+        {dots}
+      </ul>
+    )
+  }
+}
+
+Dots.propTypes = {
+  activeDot: React.PropTypes.number,
+  length: React.PropTypes.number,
+};
