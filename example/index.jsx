661169e0185e236ec54c6091e1e408f8a8598485
jkvim <jkvim@outlook.com>
2016-11-27 20:05:54 +0800

    example: add autoplay example

--- example/index.jsx
+++ example/index.jsx
@@ -3,18 +3,37 @@ import ReactDOM from 'react-dom';
 
 import { ArrowLeft, ArrowRight, Dots, Slides } from '../src';
 
+
 function App() {
   return (
     <div>
-      <Slides arrowLeft={ArrowLeft}
-              arrowRight={ArrowRight}
-              dots={Dots}
-              duration={300}
-              width="100%">
-        <div className="one" style={{background: 'green'}}></div>
-        <div className="two" style={{background: 'red'}}></div>
-        <div className="three" style={{background: 'yellow'}}></div>
-      </Slides>
+      <header>
+        <h1>React Infinite Slide</h1>
+      </header>
+      <section>
+        <h2>Slide By Hand</h2>
+        <Slides arrowLeft={ArrowLeft}
+                arrowRight={ArrowRight}
+                dots={Dots}
+                duration={300}
+                width="100%">
+          <div className="one" style={{background: '#bdff70'}}></div>
+          <div className="two" style={{background: '#f09bfd'}}></div>
+          <div className="three" style={{background: '#ffa500'}}></div>
+        </Slides>
+        <h2>Slide By Auto</h2>
+      </section>
+      <section>
+        <Slides dots={Dots}
+                duration={300}
+                width="100%"
+                autoplay
+                delay={2000}>
+          <div className="one" style={{background: '#bdff70'}}></div>
+          <div className="two" style={{background: '#f09bfd'}}></div>
+          <div className="three" style={{background: '#ffa500'}}></div>
+        </Slides>
+      </section>
     </div>
   )
 }
