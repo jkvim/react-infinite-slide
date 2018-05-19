7b05e356647dca81c0e5504232bf595887e85b54
jkvim <jkvim@outlook.com>
2018-05-19 18:05:01 +0800

    update depoly script

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,37 @@
-"use strict";
+"use strict"
 
-const gulp = require('gulp');
-const ghPages = require('gulp-gh-pages');
-const webpack = require('webpack-stream');
-const path = require('path');
+const gulp = require('gulp')
+const ghPages = require('gulp-gh-pages')
+const webpack = require('webpack-stream')
+const path = require('path')
 
 gulp.task('build', () => {
-  return gulp.src('./example/index.jsx')
+  return gulp.src('./example/index.js')
     .pipe(webpack({
       entry: {
-        bundle: path.resolve(__dirname + '/example', 'index.jsx')
+        bundle: path.resolve(__dirname + '/example', 'index.js')
       },
       output: {
         path: path.resolve(__dirname, "example"),
         filename: '[name].js',
       },
-      resolve: {
-        extensions: ['', '.js', '.jsx']
-      },
-      devtools: '#eval',
       module: {
-        loaders: [
+        rules: [
           {
-            test: /\.jsx$/,
-            loader: 'babel',
+            test: /\.js$/,
+            loader: 'babel-loader',
           }
         ],
       },
     }))
-    .pipe(gulp.dest('./example'));
-});
+    .pipe(gulp.dest('./example'))
+})
 
 // depoly to github page
 gulp.task('depoly', ['build'], () => {
   return gulp.src([
     './example/*'
-  ]).pipe(ghPages());
-});
+  ]).pipe(ghPages())
+})
 
-gulp.task('default', ['depoly']);
+gulp.task('default', ['depoly'])
